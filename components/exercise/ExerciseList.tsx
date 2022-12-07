334199de6baa0cb88e58643550e02a8e24a63635
Ethan Ayaay <ayaayethan@gmail.com>
2022-12-06 21:08:19 -0800

    Created exercise skeleton

---
+++ components/exercise/ExerciseList.tsx
@@ -0,0 +1,5 @@
+export default function ExerciseList() {
+  return (
+    <div>Exercise List</div>
+  )
+}
---
+++ pages/exercise.tsx
@@ -0,0 +1,12 @@
+import ExerciseList from "../components/exercise/ExerciseList"
+
+export default function Exercise() {
+  return (
+    <>
+      <h1 className="text-9xl font-bold underline">
+        Exercise
+      </h1>
+      <ExerciseList />
+    </>
+  )
+}
