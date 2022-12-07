3e8f59130e9ab6b1e07f97c2583edc8fcc62f612
Keenan Aldridge <kmantan@gmail.com>
2022-12-06 17:44:00 -1000

    Now renders widget

---
+++ pages/nutrition.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import CaloriesWidget from "./caloriesWidget";
+
+const Nutrition = () => {
+
+  return (
+    <CaloriesWidget />
+  )
+}
+
+export default Nutrition;
