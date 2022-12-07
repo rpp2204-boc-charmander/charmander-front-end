ae87bfd5338f4ffffaca795f0eb2a351a5474596
Riccardo Pirruccio <rickp1795@gmail.com>
2022-12-06 18:11:06 -0600

    testing github action to preview pull request branch on EC2 server

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <h1 className="text-9xl font-bold underline">
-      if you see this you rock!!!
+      This should be different now
     </h1>
   )
 }
