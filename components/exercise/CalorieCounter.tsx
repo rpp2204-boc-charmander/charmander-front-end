5c91bc5f24b53904b2ea2f2f6794c3acaa41a8a3
Ethan Ayaay <ayaayethan@gmail.com>
2022-12-08 21:14:56 -0800

    Implemented basic tailwild

--- components/exercise/CalorieCounter.tsx
+++ components/exercise/CalorieCounter.tsx
@@ -3,7 +3,7 @@ import mockData from '../../mocks/exercisedata.json';
 
 export default function CalorieCounter() {
   return (
-    <div className={styles.calories}>
+    <div className="max-w max-h-60 rounded-lg overflow-hidden shadow-lg bg-gray-100 mt-4 cursor-pointer">
       <p>{mockData.data[0].total_calories_burned} Calories Burned</p>
       <button> Add Exercise </button>
     </div>
--- components/exercise/ExerciseItem.tsx
+++ components/exercise/ExerciseItem.tsx
@@ -2,13 +2,13 @@ import styles from "../../styles/Exercise.module.css"
 
 export default function ExerciseItem ({ exercise } : any) {
   return (
-    <div className={styles.card}>
+    <div className="max-w max-h-60 rounded-lg overflow-hidden shadow-lg bg-gray-100 mx-5 mt-4 p-4 flex justify-between cursor-pointer">
       <div>
-        <h2>{exercise.name}</h2>
+        <h2 className="text-xl font-bold">{exercise.name}</h2>
         <p> Muscle Group: {exercise.muscle_group}</p>
       </div>
       <div>
-        Goals:
+        <h3 className="text-lg font-bold">Goals:</h3>
         {exercise.sets.map( (set:any) => {
           return <p key={set.id}>{set.reps} reps</p>
         })}
