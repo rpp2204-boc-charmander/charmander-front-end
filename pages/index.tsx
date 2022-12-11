09c4235f0b8c12a67fc5850cc056863b25efb1d6
Javi <javiercamposcastello@gmail.com>
2022-12-11 13:18:44 +0100

    Fixed nutrition merge with main

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Layout.module.css'
->>>>>>>>> Temporary merge branch 2
 
 export default function Home() {
   return (
