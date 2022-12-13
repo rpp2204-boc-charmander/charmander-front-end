1e830de69c899c39776d52cdabc27d1d583a1f19
Keenan Aldridge <kmantan@gmail.com>
2022-12-13 10:32:17 -1000

    Added functionality to edit/delete modal

--- components/nutrition/FoodCard.tsx
+++ components/nutrition/FoodCard.tsx
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import { BsTrash } from "react-icons/bs";
 import { AiOutlineEdit } from "react-icons/ai";
 
-export default function FoodCard ({ food } : any) {
+export default function FoodCard ({ food, setPendingItem, setIsRemoveShowing } : any) {
   const [calories, setCalories] = useState(food.CAL);
+  const [consumed, setConsumed] = useState(false);
 
   return (
     <div className="p-4 bg-white rounded-xl shadow-lg mb-4">
       <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between", height: "90px"}}>
       <div style={{display: "flex", flexDirection: "column", alignItems: "start", justifyContent: "space-between"}}>
-          <h3 className="font-bold">{food.ITEM}</h3>
+          <h3 className="w-3/5 font-bold">{food.ITEM}</h3>
           <p className="italic">Estimated calories gained: {calories}</p>
         </div>
-        {/* <div style={{height: "80px", width: "2px", backgroundColor: "black"}}></div> */}
         <div style={{display: "flex", flexDirection: "column", alignItems: "end", justifyContent: "space-between"}}>
           <div className="flex flex-row">
           <AiOutlineEdit onClick={(e) => {
@@ -20,18 +20,14 @@ export default function FoodCard ({ food } : any) {
             setCalories(servingSize * food.CAL);
           }}/>
           <BsTrash onClick={(e) => {
-            prompt(`Are you sure you want to remove ${food.ITEM}`)
+            setIsRemoveShowing(true)
+            setPendingItem(food);
           }}/>
           </div>
         <button
-        className="bg-white hover:bg-green-700 text-black text-xl py-2 px-4 rounded-full border border-black w-32 h-7 flex justify-center items-center mb-4 shadow"
-        onClick={(e) => {
-          console.log("color: ", e.target.style.backgroundColor)
-          if(e.target.style.backgroundColor = "white"){
-            e.target.style.backgroundColor = "green";
-          } else {
-            e.target.style.backgroundColor = "white";
-          }
+        className="bg-white hover:bg-green-600 text-black text-xl py-2 px-4 rounded-full border border-black w-32 h-7 flex justify-center items-center mb-4 shadow"
+        onClick={() => {
+          setConsumed(!consumed);
         }}
         >
           consumed
--- pages/nutrition.tsx
+++ pages/nutrition.tsx
@@ -3,20 +3,76 @@ import CaloriesWidget from "../components/nutrition/CaloriesWidget";
 import FoodList from "../components/nutrition/FoodList";
 import foodData from "../mocks/foodData.json";
 import { GiForkKnifeSpoon } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
+import { getDisplayName } from "next/dist/shared/lib/utils";
 
 const Nutrition = () => {
+  const [pendingItem, setPendingItem] = useState('');
+  const [isRemoveShowing, setIsRemoveShowing] = useState(false);
+  const [allFoods, setAllFoods] = useState(foodData);
+
+const RemoveItemModal = ({pendingItem}) => {
+  const modalStyling = {
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "#FFF",
+    padding: "50px",
+    zIndex: "1000"
+  }
 
   return (
     <>
-    <div className="flex flex-row mb-10">
-    <GiForkKnifeSpoon className="text-3xl mr-5"/>
-    <div className="text-3xl">Nutrition</div>
-    <p style={{marginLeft: "400px"}}>{`< 10 Dec 2022 >`}</p>
-    </div>
-    <div className="flex flex-row">
-      <CaloriesWidget foodData={foodData}/>
-      <FoodList foodData={foodData}/>
-    </div>
+      <div style={modalStyling} className="shadow-2xl">
+        {/* <GrClose className="absolute top-2 right-2"/> */}
+        <p>{`Are you sure you want to remove ${pendingItem.ITEM}?`}</p>
+        <div className="p-10 flex flex-row justify-center">
+          <button className="mr-2 bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 hover:border-transparent rounded"
+          onClick={() => {
+            let newFoodsList = allFoods;
+            for(let item in newFoodsList){
+              if(newFoodsList[item].ITEM === pendingItem.ITEM){
+                delete newFoodsList[item];
+              }
+            }
+            setAllFoods(newFoodsList);
+            setIsRemoveShowing(false);
+          }}>
+            Confirm
+          </button>
+          <button className="ml-2 bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded"
+          onClick={() => setIsRemoveShowing(false)}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+  return (
+    <>
+      <div className="flex justify-between flex-row mb-10 w-auto">
+        <div className="flex flex-row">
+          <GiForkKnifeSpoon className="text-3xl mr-2"/>
+          <div className="text-3xl">Nutrition</div>
+        </div>
+        <div className="inline-flex">
+          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
+            Prev
+          </button>
+          <p className="flex items-center">Today</p>
+          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">
+            Next
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-row justify-between p-2 w-auto">
+        <CaloriesWidget foodData={allFoods}/>
+          {isRemoveShowing ? <RemoveItemModal pendingItem={pendingItem}/> : null}
+        <FoodList foodData={allFoods} setPendingItem={setPendingItem} setIsRemoveShowing={setIsRemoveShowing}/>
+      </div>
     </>
   )
 }
