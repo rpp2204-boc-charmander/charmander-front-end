51e5841c1bd989960d1d8209e1a2bc857bcd3619
kopeda <kristopher.opeda@gmail.com>
2022-12-17 09:31:46 -0800

    Redid styling according to Tailwind conventions.

--- components/login/SignupModal.tsx
+++ components/login/SignupModal.tsx
@@ -14,6 +14,7 @@ export default function SignupModal() {
   const [weight, setWeight] = useState(0);
   const [sex, setSex] = useState('');
 
+  // Old Inline-Style
   const modalStyle = {
     "display": "flex",
     "flex-direction": "column",
@@ -28,54 +29,26 @@ export default function SignupModal() {
     "border-radius": "20px"
   };
 
-  const signUpStyle = {
-    "width": "53px",
-    "height": "auto",
-    "font-style": "normal",
-    "font-weight": 900,
-    "font-size": "16px",
-    "line-height": "19px",
-    "color": "#000000",
-    "flex": "auto",
+  // Non-working Style
+  const twModalStyle = "flex flex-col items-start pt-27 pr-25 gap-11 relative w-404 h-auto bg-LoginGray rounded-3xl shadow-md";
 
-    "flex-grow": 0,
-  }
+  // Working Tailwind Styles
+  const twSignUpStyle = "w-53 h-auto font-sans not-italic font-bold text-base leading-5 decoration-black flex-auto grow-0";
+  const twInputStyle = "w-full h-10 boxShadow-wellShadow flex-none grow-0 rounded-xl indent-3";
+  const twSubmitStyle = "w-53 h-auto font-sans not-italic font-bold text-base leading-5 decoration-black flex-auto grow-0 underline underline-offset-4";
 
-  const inputStyle = {
-    "width": "354px",
-    "height": "38px",
-    "box-shadow": "inset 0px 2px 2px rgba(0, 0, 0, 0.25)",
-    "flex": "none",
-    "flex-grow": 0,
-    "border-radius" : "12px",
-    "text-indent" : "10px"
-  }
-
-  const submitStyle = {
-    "width": "53px",
-    "height": "auto",
-    "font-style": "normal",
-    "font-weight": 900,
-    "font-size": "16px",
-    "line-height": "19px",
-    "color": "#000000",
-    "flex": "auto",
-    "order": 0,
-    "flex-grow": 0,
-    "text-decoration-line" : "underline",
-  }
 
   return (
     <div style={modalStyle}>
-      <div style={signUpStyle}>signup</div>
-      <input style={inputStyle} placeholder="first name"></input>
-      <input style={inputStyle} placeholder="last name"></input>
-      <input style={inputStyle} placeholder="email name"></input>
-      <input style={inputStyle} placeholder="username"></input>
-      <input style={inputStyle} placeholder="password"></input>
-      <input style={inputStyle} placeholder="height (in.)"></input>
-      <input style={inputStyle} placeholder="weight (lbs.)"></input>
-      <input style={inputStyle} placeholder="sex"></input>
-      <a style={submitStyle} href="/overview">submit</a>
+      <div className={twSignUpStyle}>signup</div>
+      <input className={twInputStyle} onChange={(e) => {setFirstName(e.target.value)}} placeholder="first name"></input>
+      <input className={twInputStyle} onChange={(e) => {setLastName(e.target.value)}} placeholder="last name"></input>
+      <input className={twInputStyle} onChange={(e) => {setEmail(e.target.value)}} placeholder="email name"></input>
+      <input className={twInputStyle} onChange={(e) => {setUsername(e.target.value)}} placeholder="username"></input>
+      <input className={twInputStyle} onChange={(e) => {setPassword(e.target.value)}} placeholder="password"></input>
+      <input className={twInputStyle} onChange={(e) => {setHeight(Number(e.target.value))}} placeholder="height (in.)"></input>
+      <input className={twInputStyle} onChange={(e) => {setWeight(Number(e.target.value))}} placeholder="weight (lbs.)"></input>
+      <input className={twInputStyle} onChange={(e) => {setSex(e.target.value)}} placeholder="sex"></input>
+      <a className={twSubmitStyle} href="/overview">submit</a>
     </div>)
 }
