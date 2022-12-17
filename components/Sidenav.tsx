85345f72ebc06f26d5201e1b0ec6f615e1fdc514
Keenan Aldridge <kmantan@gmail.com>
2022-12-17 07:54:55 -1000

    Fixing CI/CD error

--- components/Sidenav.tsx
+++ components/Sidenav.tsx
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
 import styles from '../styles/Sidenav.module.css';
-=======
-import { MdAccountCircle, MdOutlineSort, MdOutlineFitnessCenter, MdRestaurant, MdOutlineAssignment, MdOutlinePeopleAlt, MdOutlineSettings, MdLogout } from "react-icons/md";
-
->>>>>>> 616cc18 (Sidebar using tailwind)
 import Link from 'next/link';
 
 export default function Sidenav() {
   return (
-<<<<<<< HEAD
       <div className={styles.sidenav}>
 
         <div className={styles.userinfo}>
@@ -42,71 +36,6 @@ export default function Sidenav() {
             </li>
           </ul>
         </div>
-=======
-    <div className="flex flex-col grow bg-gray-500 h-[100vh] w-[200px] fixed top-0 left-0 overflow-x-hidden items-center text-xl justify-between pt-10">
-      <div className="flex flex-col items-center">
-        <div className="pb-1">
-          <MdAccountCircle className="h-32 w-32" />
-        </div>
-
-        <div className='username text-xl pb-2'> @username </div>
-
-        <div className='names text-sm italic'>
-          FirstName LastName </div>
-      </div>
-
-      <ul className="w-32 flex flex-col grow max-h-[35rem] justify-between pt-10 pb-[12rem] font-bold">
-        <li>
-        <Link className="flex flex-row items-center hover:text-yellow-400" href='/'>
-            <MdOutlineSort className="h-5 w-5 mr-2" />
-            Overview
-          </Link>
-        </li>
-
-        <li>
-          <Link className="flex flex-row items-center hover:text-yellow-400" href='/'>
-            <MdOutlineFitnessCenter className="h-5 w-5 mr-2" />
-            Exercise
-          </Link>
-        </li>
-
-        <li>
-          <Link className="flex flex-row items-center hover:text-yellow-400" href='/'>
-            <MdRestaurant className="h-5 w-5 mr-2" />
-            Nutrition
-          </Link>
-        </li>
-
-        <li>
-          <Link className="flex flex-row items-center hover:text-yellow-400" href='/'>
-            <MdOutlineAssignment className="h-5 w-5 mr-2" />
-            Report
-          </Link>
-        </li>
-
-        <li>
-          <Link className="flex flex-row items-center hover:text-yellow-400" href='/'>
-            <MdOutlinePeopleAlt className="h-5 w-5 mr-2" />
-            Friends
-          </Link>
-        </li>
-
-        <li>
-          <Link className="flex flex-row items-cente hover:text-yellow-400" href='/'>
-            <MdOutlineSettings className="h-5 w-5 mr-2" />
-            Settings
-          </Link>
-        </li>
-      </ul>
-
-
-      <div className="w-32 flex pb-10 font-bold hover:text-yellow-400">
-        <Link className="flex flex-row items-center" href='/'>
-          <MdLogout className="h-5 w-5 mr-2" />
-          Logout
-        </Link>
-      </div>
->>>>>>> 616cc18 (Sidebar using tailwind)
     </div>
   )
 };
