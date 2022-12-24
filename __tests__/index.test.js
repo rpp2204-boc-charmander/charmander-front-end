8cd3bf6238532c15d79f1079f020d0dfd583ab4c
Riccardo Pirruccio <rickp1795@gmail.com>
2022-12-23 18:39:25 -0600

    updated github actions naming conventions

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,38 +4,26 @@ import Home from '../pages/index';
 
 import '@testing-library/jest-dom';
 
-// describe('Home', () => {
-//   it('renders a heading', () => {
-//     render(<Home />);
-
-//     const heading = screen.getByRole('heading', {
-//       name: /welcome to next\.js!/i,
-//     });
-
-//     expect(heading).toBeInTheDocument();
-//   });
-// });
-
 describe('Exercise', () => {
-  it ('renders an exercise heading', () => {
+  it('renders an exercise heading', () => {
     render(<Exercise />);
 
     const heading = screen.getByRole('heading', {
-      name: /exercise/i
+      name: /exercise/i,
     });
 
     expect(heading).toBeInTheDocument();
-  })
+  });
 
-  it ('renders an exercise item', () => {
+  it('renders an exercise item', () => {
     render(<Exercise />);
 
     const item = screen.getByRole('heading', {
-      name: /pull ups/i
+      name: /pull ups/i,
     });
 
     expect(item).toBeInTheDocument();
-  })
+  });
 });
 
 it('adding 1 + 1 returns 2', () => {
