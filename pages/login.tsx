10d3792d9131e55c1e437281265d06e3f0b2cfc0
ChiElephant <anthony.merino@me.com>
2022-12-29 16:01:37 -0700

    adding setting page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,8 @@
-/* gloabal google */
-import Head from "next/head";
-import LoginCard from "../components/login/LoginCard";
-import { IoIosFitness } from "react-icons/io";
-import { MdOutlineFastfood } from "react-icons/md";
+import Head from 'next/head';
+import LoginCard from '../components/login/LoginCard';
+import { IoIosFitness} from 'react-icons/io';
+import { MdOutlineFastfood } from 'react-icons/md'
+import { FitbitProvider } from '../context/FitbitProvider'
 
 const Login = () => {
   const titleLg =
@@ -11,30 +11,29 @@ const Login = () => {
   const exerciseIconLg = "test-7xl";
 
   return (
-    <div>
-      <Head>
-        <title>My Health Coach</title>
-        <meta
-          name="login"
-          content="My Health Coach Login, Fitness, Nutrition, Exercise"
-        />
-        <link rel="icon" href="/flavion.ico" />
-      </Head>
-      <main>
+    <FitbitProvider>
+      <div>
+        <Head>
+          <title>My Health Coach</title>
+          <meta name='login' content="My Health Coach Login, Fitness, Nutrition, Exercise" />
+          <link rel='icon' href="/flavion.ico" />
+        </Head>
+        <main>
         <div className={titleLg}>
           <h1>My Heath Coach</h1>
-          <div className="mt-3 flex w-full flex-row">
+          <div className="flex flex-row w-full mt-3">
             <div className="grow"></div>
             <MdOutlineFastfood className={foodIconLg} />
             <div className="grow"></div>
             <IoIosFitness className={foodIconLg} />
             <div className="grow"></div>
           </div>
-        </div>
-        <LoginCard />
-      </main>
-    </div>
-  );
-};
+        </div >
+          <LoginCard />
+        </main>
+      </div>
+    </FitbitProvider>
+  )
+}
 
 export default Login;
