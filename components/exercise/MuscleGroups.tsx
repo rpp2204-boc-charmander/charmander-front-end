aa37b8b2aa89f4fec6afcde97d706f23f8a061f9
Riccardo Pirruccio <rickp1795@gmail.com>
2022-12-29 12:26:50 -0600

    turned off typescript linting rules to not interfere with docker build

--- components/exercise/MuscleGroups.tsx
+++ components/exercise/MuscleGroups.tsx
@@ -4,7 +4,7 @@ const MuscleGroups = ({
 }: any): JSX.Element => {
   return (
     <ul className="y-1 m-2 flex flex-row content-center space-x-4">
-      {muscle_groups.map((item) => {
+      {muscle_groups.map((item: any) => {
         return (
           <li
             className="w-30 rounded border border-gray-400 bg-white py-2 px-4 font-semibold text-gray-800 shadow hover:bg-gray-100"
