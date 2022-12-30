654d32e6192b5e7c21f2947e835a12f3a4b12b6c
ChiElephant <anthony.merino@me.com>
2022-12-29 19:41:55 -0700

    settings form

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import { MdOutlineSettings } from 'react-icons/md'
-import UserExperience from '../components/settings/UserExperience'
-import UserMetrics from '../components/settings/UserMetrics'
-import Head from "next/head";
+import Header from '../components/overview/Header'
 
 export default function Settings () {
  return (
+
   <div>
-    <Head>
-      <title> My Health Coach </title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <div>
+      <div className='absolute w-[1066px] h-[278px] left-[284px] top-[208px] font-bold text-3xl'>
+        <div className='absolute w-[1066px] h-[58px] bg-[#8A8A8A] shadow-lg rounded-t-2xl text-[#FFFFFF] pl-8'>
+            user metrics
+        </div>
+      </div>
+      <div className='absolute w-[1066px] h-[237px] left-[284px] top-[249px] bg-[#D9D9D9] shadow-lg rounded-b-2xl'></div>
+    </div>
 
-    <UserMetrics />
-    <UserExperience />
+    <div>
+      <div className='absolute w-[1066px] h-[172px] left-[284px] top-[522px] font-bold text-3xl'>
+        <div className='absolute w-[1066px] h-[58px] bg-[#8A8A8A] shadow-lg rounded-t-2xl text-[#FFFFFF] pl-8'>
+            user experience
+        </div>
+      </div>
+      <div className='absolute w-[1066px] h-[172px] left-[284px] top-[563px] bg-[#D9D9D9] shadow-lg rounded-b-2xl'></div>
+    </div>
   </div>
-
  )
 }
