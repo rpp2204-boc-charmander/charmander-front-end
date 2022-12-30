cefda288d7dda6581d2f4fa039d6b208e4bb7010
Ethan Ayaay <ayaayethan@gmail.com>
2022-12-30 01:51:56 -0800

    Refactored add set modal to allow for multiple sets to be added

--- components/exercise/AddSet.tsx
+++ components/exercise/AddSet.tsx
@@ -5,17 +5,24 @@ import axios from "axios";
 
 export default function AddSet({ toggleAddSetModal, workoutID }: any) {
   const [ confirm, setConfirm ] = useState(false);
+  const [ setNumber , setSetNumber ] = useState(0);
 
-  const weightRef: any = useRef();
-  const repsRef: any = useRef();
+  const weightRef: any = useRef([]);
+  const repsRef: any = useRef([]);
 
   const onSubmit = () => {
-    let weight_lbs = Number(weightRef.current.value);
-    let reps = Number(repsRef.current.value);
 
-    if (weight_lbs && reps) {
+    let reps = repsRef.current.map( (rep: any) => {
+      return Number(rep?.value);
+    })
+
+    let weights = weightRef.current.map( (weight: any) => {
+      return Number(weight?.value);
+    })
+
+    if (reps[0] && weights[0]) {
       axios.post('api/exercise/create/set', {
-          weight_lbs,
+          weights,
           reps,
           workout_exercise_id: workoutID
         })
@@ -41,7 +48,7 @@ export default function AddSet({ toggleAddSetModal, workoutID }: any) {
 
 
       <div
-        className="fixed top-[50%] left-[50%] z-50 flex h-[35%] w-[40%] translate-x-[-50%]
+        className="fixed top-[50%] left-[50%] z-50 flex h-[45%] w-[40%] translate-x-[-50%]
       translate-y-[-50%] flex-col items-center rounded-3xl bg-gray-300 pl-10 pr-10 text-black">
 
         <div className="header flex w-[100%] flex-row items-center justify-between pt-4 pb-4">
@@ -52,36 +59,58 @@ export default function AddSet({ toggleAddSetModal, workoutID }: any) {
           />
         </div>
 
-        <div className="bg-gray-500 flex flex-col rounded-2xl h-[50%] w-full justify-around items-center overflow-y-scroll no-scrollbar shadow-well">
+        <div className="bg-gray-500 flex flex-col rounded-2xl h-[60%] w-full justify-around items-center overflow-y-scroll no-scrollbar shadow-well">
 
           <div className="bg-slate-100 flex flex-col h-5/6 w-11/12 py-3 rounded-2xl shadow-lg justify-evenly items-center relative">
 
-          { confirm && <p className="absolute text-red-500 top-0"> Please Set Reps and Weight </p> }
+          { confirm && <p className="absolute text-red-500 top-0 text-sm"> Please Set Reps and Weight </p> }
 
-              <div className="flex w-[290px]">
-                <h3 className="w-20 text-center font-bold"> Reps </h3>
+          { setNumber < 1 &&
+            <>
+              <select name="sets" onChange={ (e) => { setSetNumber(Number(e.target.value)) }} className="bg-slate-50 rounded-lg p-5 w-[70%] shadow-xl text-center font-bold">
+                <option selected={true}> Choose Number of Sets</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+            </>
+          }
+
+          { setNumber > 0 && Array(setNumber).fill(1).map( (x, i) => {
+            return (
+            <div key={i} className="flex justify-evenly w-[80%]">
+              <div className="flex">
+                  <h3 className="w-20 text-center font-bold"> Reps </h3>
+                  <input
+                    className="rounded-lg shadow-md bg-slate-50 w-[100px]"
+                    type="number"
+                    ref={(e) => { repsRef.current[i] = e }}
+                  ></input>
+                </div>
+
+              <div className="flex">
+                <h3 className="w-20 text-center font-bold"> Weight </h3>
                 <input
-                  className="rounded-lg shadow-md bg-slate-50"
+                  className="rounded-lg shadow-md bg-slate-50 w-[100px]"
                   type="number"
-                  ref={repsRef}
+                  ref={(e) => { weightRef.current[i] = e }}
                 ></input>
+                <span className="ml-3"> lbs </span>
               </div>
-
-            <div className="flex">
-              <h3 className="w-20 text-center font-bold"> Weight </h3>
-              <input
-                className="rounded-lg shadow-md bg-slate-50"
-                type="number"
-                ref={weightRef}
-              ></input>
-              <span className="ml-3"> lbs </span>
             </div>
+            )
+          })}
 
           </div>
 
          </div>
 
-        <button className="bg-blue-500 hover:bg-blue-600 text-slate-50 rounded-full w-full px-10 py-4 font-bold mt-4 shadow-md" onClick={onSubmit}> Add </button>
+        <button className="bg-blue-500 hover:bg-blue-600 text-slate-50 rounded-full
+                             w-full px-10 py-4 font-bold mt-4 shadow-md"
+                onClick={onSubmit}
+                disabled={ setNumber === 0 ? true : false}> Add </button>
 
       </div>
 
--- pages/api/exercise/create/set.ts
+++ pages/api/exercise/create/set.ts
@@ -2,9 +2,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 
 export default async function handler(req: any, res: any) {
-  let { weight_lbs, reps, workout_exercise_id  } = req.body;
+  let { weights, reps, workout_exercise_id  } = req.body;
 
-  return axios.post(`${process.env.API_URL}/exercise/create/set?weight_lbs=${weight_lbs}&reps=${reps}&workout_exercise_id=${workout_exercise_id}`)
+  return axios.post(`${process.env.API_URL}/exercise/create/set`, {
+            weights,
+            reps,
+            workout_exercise_id
+          })
           .then(({ data }) => {
             res.status(200).send(`Successfuly Added Set to workout ${workout_exercise_id}`);
           })
