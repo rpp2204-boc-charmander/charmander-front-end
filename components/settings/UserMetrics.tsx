59aeb51ca6bce1b51b2bc848ebe83e75dc78f42f
huchchas <hsuchchas@gmail.com>
2022-12-30 22:06:26 -0500

    Added settings page

--- components/settings/UserMetrics.tsx
+++ components/settings/UserMetrics.tsx
@@ -1,11 +1,76 @@
-import React from 'react'
+import { useState } from "react"
 
-type Props = {}
+export default function UserMetrics() {
+  const [userMetrics, setUserMetrics] = useState <any> ({
+    height: 0,
+    weight: 0,
+    age: 0,
+    gender: ""
+  })
 
-function UserMetrics({}: Props) {
+  const twSettingsInputClass = "w-[30vw] min-h-[1.5rem] lg:w-[20rem] lg:h-12 shadow rounded-xl indent-3 bg-slate-100 focus:outline-none text-black";
+
+  const handleOnChange = function(e: any) {
+    e.preventDefault();
+    setUserMetrics({
+      ...userMetrics,
+      [e.target.name]: e.target.value
+    })
+  };
+
+  const handleSubmit = function() {
+
+  };
+
   return (
-    <div>UserMetrics</div>
+    <form className="flex w-full h-full items-center">
+      <div className="flex flex-col w-full h-full items-center justify-between space-y-3 lg:space-y-5">
+        <div className="flex flex-row space-x-5">
+          <input
+            type="text"
+            name="height"
+            placeholder="height"
+            className={twSettingsInputClass}
+            onChange={handleOnChange}
+          >
+          </input>
+
+          <input
+            type="text"
+            name="weight"
+            placeholder="weight"
+            className={twSettingsInputClass}
+            onChange={handleOnChange}
+          >
+          </input>
+        </div>
+
+        <div className="flex flex-row space-x-5">
+          <input
+            type="text"
+            name="age"
+            placeholder="age"
+            className={twSettingsInputClass}
+            onChange={handleOnChange}
+          >
+          </input>
+
+          <input
+            type="text"
+            name="gender"
+            placeholder="gender"
+            className={twSettingsInputClass}
+            onChange={handleOnChange}
+          >
+          </input>
+        </div>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-[10rem]">
+          Submit
+        </button>
+      </div>
+    </form>
   )
 }
 
-export default UserMetrics
+ UserMetrics
