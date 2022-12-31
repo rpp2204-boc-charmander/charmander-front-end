b4039fe26580735dad10e36ed599e632cc372669
huchchas <hsuchchas@gmail.com>
2022-12-31 01:18:43 -0500

    Fixed overview cards distribution

--- components/overview/Container.tsx
+++ components/overview/Container.tsx
@@ -88,8 +88,7 @@ export default function Container({
       ></Modal>
 
       <div className="relative w-full h-full">
-        <div className="z-0 bg-gray-300 dark:bg-slate-300 flex flex-row min-h-full lg:h-[16rem] justify-evenly
-          items-center bg-fixed overflow-x-scroll pl-2 pr-2 lg:pl-[4rem] lg:pr-[4rem] scrollbar-hide border-r border-l w-full"
+        <div className={`z-0 bg-gray-300 dark:bg-slate-300 flex flex-row min-h-full lg:h-[16rem] ${type === 'calories'? 'justify-between' : 'justify-right'} items-center bg-fixed overflow-x-scroll pl-2 pr-2 lg:pl-[4rem] lg:pr-[4rem] scrollbar-hide border-r border-l w-full`}
         >
           {cards.length === 0 && type === "exercise" && (
             <div className="flex w-full justify-center">
